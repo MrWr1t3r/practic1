26bd0a13c32b50f228a8ac7b0ce206816b6a0ac4
MrWr1t3r <37981289+MrWr1t3r@users.noreply.github.com>
2018-04-03 04:54:34 +0300

    alert( positiveSalaries );

--- practic1.js
+++ practic1.js
@@ -1,3 +1,10 @@
+// .sort - сортировка. Если нужно установить свой порядок - вводим перед массивом функцию и задаем ей параметры. Пример: 
+//  function compareNumeric(a, b) {
+//    if (a > b) return 1;
+//    if (a < b) return -1;
+//  }
+
+
 function compareNumeric(a, b) {
   if (a > b) return 1;
   if (a < b) return -1;
@@ -5,5 +12,7 @@ function compareNumeric(a, b) {
 
 let salaries = [1700, 2000, 1560, 2300, 1920, 1400];
 
-salaries.sort(compareNumeric);
-
+//salaries.sort(compareNumeric);
+var positiveSalaries = salaries.filter(function(number) {
+return number > 1450;
+});
