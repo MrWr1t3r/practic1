3a568bff298c2a1431d3274228bb71c80985a167
MrWr1t3r <37981289+MrWr1t3r@users.noreply.github.com>
2018-04-03 04:42:52 +0300

    Update practic1.js

--- practic1.js
+++ practic1.js
@@ -1,1 +1,9 @@
-let salaries = [1700, 2000, 1560, 2300, 1920, 1400]
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+
+let salaries = [1700, 2000, 1560, 2300, 1920, 1400];
+
+salaries.sort(compareNumeric);
+
