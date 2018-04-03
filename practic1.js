6342cfa170840a9b64ba7538ad825ac837c0010b
MrWr1t3r <37981289+MrWr1t3r@users.noreply.github.com>
2018-04-03 05:21:12 +0300

    Update practic1.js

--- practic1.js
+++ practic1.js
@@ -12,7 +12,18 @@ function compareNumeric(a, b) {
 
 let salaries = [1700, 2000, 1560, 2300, 1920, 1400];
 
-//salaries.sort(compareNumeric);
-var positiveSalaries = salaries.filter(function(number) {
-return number > 1450;
-});
+// salaries.sort(compareNumeric);
+
+//  var positiveSalaries = salaries.filter(function(number) {
+//    return number > 1450;
+//  });
+
+min = salaries[0];
+max = min;
+
+for( i=0; i<salaries.length; ++i ) {
+  if (salaries[i] > max) max = salaries[i];
+      if (salaries[i] < min) min = salaries[i];
+}
+
+
